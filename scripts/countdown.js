f6d5c22a8c7a1394549f6a4c23036e0f9b4086c5
RJgTH <60988872+RJgTH@users.noreply.github.com>
2020-02-15 06:24:52 +0000

    8

---
+++ scripts/countdown.js
@@ -0,0 +1,20 @@
+var countDown = function () {
+    document.getElementById('countdown') = days + "d " + hours + "h " +
+        minutes + "m " + seconds + "s "
+}
+
+var countDownDate = new Date("May 25, 2020 00:00:00").getTime();
+var x = setInterval(function () {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("countdown").innerHTML = days + "d " + hours + "h " +
+        minutes + "m " + seconds + "s ";
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "WOO-HOO!! We made it!";
+    }
+}, 1000);
